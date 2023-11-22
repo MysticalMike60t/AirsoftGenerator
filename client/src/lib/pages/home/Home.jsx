0dd6fe530ab72cce3cf7378af4980371e6d542fc
Mysticmike60t <caden.finkelstein@cttech.org>
2023-11-22 14:45:45 -0500

    sd

--- client/src/lib/pages/home/Home.jsx
+++ client/src/lib/pages/home/Home.jsx
@@ -3,16 +3,18 @@ import { Link, useLocation } from 'react-router-dom';
 
 const Home = () => {
   const location = useLocation();
-  
+
   const presetViewerBodyImages = {
-    ar15: 'ar15-image-url',
-    ak: 'ak-image-url',
-    smg: 'smg-image-url',
+    ar15: '/assets/images/rifles/ar15/Ferfrans Fully Licensed M4 AEG.png',
+    ak: '/assets/images/rifles/ak/ELAK104 AEG ESSENTIAL.png',
+    smg: '/assets/images/smgs/mp7/HK MP7 Navy GBB Airsoft Submachine Gun.png',
   };
 
   const viewerBodyCategory = new URLSearchParams(location.search).get('image');
 
   const viewerBodyUrl = presetViewerBodyImages[viewerBodyCategory] || 'default-image-url';
+
+  const sightSelectorClass = viewerBodyCategory ? `sight-selector ${viewerBodyCategory}` : 'sight-selector';
 
   return (
     <div className="page home">
@@ -23,7 +25,10 @@ const Home = () => {
       </div>
       <div className="viewer">
         <div className="wrapper">
-          <img src={viewerBodyUrl} alt="Body" className="body" />
+          <div className="gun">
+            <div className={sightSelectorClass}></div>
+            <img src={viewerBodyUrl} alt="Body" className="body" />
+          </div>
         </div>
       </div>
     </div>
