cfcdb74c2fda3a20e4c7cb26439afe05f30da404
Mysticmike60t <caden.finkelstein@cttech.org>
2023-11-22 17:27:48 -0500

    Update Header.jsx

--- client/src/lib/global/components/Header.jsx
+++ client/src/lib/global/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header" id="header">
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <a href="/">Home</a>
         </li>
         {/* <li>
           <Link to="/">Placeholder</Link>
